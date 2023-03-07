e3d930064a1af79b2ca8771f6ebb8a475c06cf50
u2wish4 <118094375+u2wish4@users.noreply.github.com>
2023-03-07 11:13:09 -0800

    update to new trading logic from tradingbot2

---
+++ main.py
@@ -0,0 +1,33 @@
+import backtrader as bt
+import pandas as pd
+import quandl
+
+quandl.ApiConfig.api_key = 'Ue5ZXwiprG4dVywqsSxf' # replace with your Quandl API key
+
+class SimpleStrategy(bt.Strategy):
+    
+    def __init__(self):
+        self.data_close = self.datas[0].close
+        self.sma20 = bt.indicators.SimpleMovingAverage(self.datas[0], period=20)
+        self.sma50 = bt.indicators.SimpleMovingAverage(self.datas[0], period=50)
+        
+    def next(self):
+        if self.sma20 > self.sma50:  # If the 20-day moving average is above the 50-day moving average
+            self.buy()  # Buy one share
+        elif self.sma20 < self.sma50:  # If the 20-day moving average is below the 50-day moving average
+            self.sell()  # Sell all shares
+        
+cerebro = bt.Cerebro()
+
+data = bt.feeds.PandasData(dataname=quandl.get('WIKI/AAPL', start_date='2016-01-01', end_date='2021-12-31'))
+
+
+cerebro.adddata(data)
+cerebro.addstrategy(SimpleStrategy)
+
+cerebro.broker.setcash(10000)
+cerebro.broker.setcommission(commission=0.001)
+
+cerebro.run()
+
+print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
